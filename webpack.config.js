fede9c96c383fcc831fc611b60004aedc5b7740b
zenitovich <zenitovich@gmail.com>
2022-08-23 16:00:38 +0300

    Default settings 1.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: './src/main.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin(),
+        new CleanWebpackPlugin()
+    ]
 }
