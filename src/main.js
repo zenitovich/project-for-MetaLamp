0aca2f2dfbeedc45bd0915c39a707e0d75f75f7f
zenitovich <zenitovich@gmail.com>
2022-09-23 15:32:13 +0300

    page1 with true calendar

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import "./styles/components/header.scss"
 import "./styles/components/footer.scss"
 import "./styles/pages/index.scss"
+import "air-datepicker/air-datepicker.css";
+import "./pages/scripts/index.js"
---
+++ src/pages/scripts/index.js
@@ -0,0 +1,23 @@
+import AirDatepicker from 'air-datepicker';
+let button = {
+    content: " Применить",
+    className: "custom-button-classname",
+    onClick: (dp) => {
+    dp.hide();
+    },
+    };
+    
+    new AirDatepicker("#arrival", {
+    buttons: [button, "clear"], // Custom button, and pre-installed 'clear' button
+    // range: true,
+    // inline: true,
+    });
+    
+    new AirDatepicker("#departure", {
+    buttons: [button, "clear"], // Custom button, and pre-installed 'clear' button
+    });
+    
+    new AirDatepicker("#data-registration", {
+    buttons: [button, "clear"], // Custom button, and pre-installed 'clear' button
+    }); 
+    console.log('Den molodec')
