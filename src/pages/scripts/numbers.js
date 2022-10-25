42805bdf5d6622fc2dff0adf890793ee9c831bca
zenitovich <zenitovich@gmail.com>
2022-10-25 13:08:43 +0300

    page2 v.1.2.0

---
+++ src/pages/scripts/numbers.js
@@ -0,0 +1,4 @@
+document.getElementById('addAm').onclick = function () {
+    document.getElementById('numContNamSel').classList.toggle('numbers-container-name__select--block')
+    document.getElementById('downArrow').classList.toggle('up-arrow')
+}
